c82daa9332a10b659a36541384dd48c6d88703a8
Jakson Casas <jaksoncasas@gmail.com>
2021-09-15 15:51:03 -0500

    Add Company models.py

--- companyAPI/models.py
+++ companyAPI/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=10)
+    email = models.CharField(max_length=30)
+    website = models.CharField(max_length=80)
