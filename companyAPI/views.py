920ec0405d61081a07663939fd76af8d09240969
Jakson Casas <jaksoncasas@gmail.com>
2021-09-15 15:59:10 -0500

    Crear funcion GET y agregar la libreria JsonResponse

--- companyAPI/views.py
+++ companyAPI/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Company
+from django.http import JsonResponse
 
 # Create your views here.
+class CompanyView(View):
+    def get(self,request):
+        #ger
+        list = Company.objects.all()
+        return JsonResponse(list,False) #Va devolver un array de objetos Json
+
+    def post(self,request):
+        #post
+    def put(self,request):
+        #put
+    def delete(self,request):
+        #delete
